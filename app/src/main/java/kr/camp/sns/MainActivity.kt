eecb13715fa57d7ee758e5ddd9918574d4e4fac7
WinterWood0118 <dltjdals125@gmail.com>
2024-04-01 20:57:22 +0900

    Add - 아이콘 등 각종 main 리소스 Update - Todo List        - activity_main.xml

--- app/src/main/java/kr/camp/sns/MainActivity.kt
+++ app/src/main/java/kr/camp/sns/MainActivity.kt
@@ -11,5 +11,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
     }
 }
+
+
+
+/*
+1. 메인 텍스트뷰 내부의 글: username + post 인데 spannableString 객체를 이용해 포스트는 노말, 유저네임은 볼드로 바꾸기
+2. 리사이클러 뷰 어댑터 만들기
+3. 유저 추천?은 리사이클러 뷰 쓸지 그냥 include 상태로 내버려 둘 지 고민
+4. 원 내부에
+
+
+
+ */
