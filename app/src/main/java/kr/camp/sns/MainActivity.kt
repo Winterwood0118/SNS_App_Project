9b1773cf3d7fec5723ef79725189c3c4644a6ff0
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-04-01 15:58:24 +0900

    feat: 뷰바인딩으로 변경

--- app/src/main/java/kr/camp/sns/MainActivity.kt
+++ app/src/main/java/kr/camp/sns/MainActivity.kt
@@ -2,10 +2,14 @@ package kr.camp.sns
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kr.camp.sns.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
