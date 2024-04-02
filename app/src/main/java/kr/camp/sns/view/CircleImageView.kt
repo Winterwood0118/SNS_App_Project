c6f0927f7a5f444198bb9b1c03f55978e340086b
JUN <treeralph@gmail.com>
2024-04-02 19:55:14 +0900

    CircleImageView & XML

--- app/src/main/java/kr/camp/sns/view/CircleImageView.kt
+++ app/src/main/java/kr/camp/sns/view/CircleImageView.kt
@@ -1,27 +1,95 @@
 package kr.camp.sns.view
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
-import android.view.View
+import android.util.Log
+import android.view.Gravity
 import android.widget.FrameLayout
+import android.widget.ImageView
 import androidx.cardview.widget.CardView
-import androidx.core.view.children
 import kr.camp.sns.R
+import kotlin.math.min
 
-class CircleImageView: FrameLayout {
+class CircleImageView : FrameLayout {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        background = resources.getDrawable(R.drawable.gradient_color)
-        val layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-        val cardView = CardView(context)
-        cardView.layoutParams = layoutParams
-        cardView.radius = layoutParams.width.toFloat() / 2
-        cardView.
-        addView(cardView)
+
+        val a = context.obtainStyledAttributes(attrs, R.styleable.CircleImageView)
+
+        val imageResourceId = a.getResourceId(
+            R.styleable.CircleImageView_android_src, -1)
+        val attrsWidthPx = a.getDimension(
+            R.styleable.CircleImageView_android_layout_width, 0f)
+        val attrsHeightPx = a.getDimension(
+            R.styleable.CircleImageView_android_layout_height, 0f)
+
+        val outerBorderSize = min(attrsWidthPx, attrsHeightPx)
+        val innerBorderSize = outerBorderSize - outerBorderSize / 15
+        val imageBorderSize = outerBorderSize * 5 / 6
+
+        val outerLayoutParams = LayoutParams(
+            outerBorderSize.toInt(),
+            outerBorderSize.toInt()
+        ).apply {
+            gravity = Gravity.CENTER
+        }
+
+        val innerLayoutParams = LayoutParams(
+            innerBorderSize.toInt(),
+            innerBorderSize.toInt()
+        ).apply {
+            gravity = Gravity.CENTER
+        }
+
+        val imageLayoutParams = LayoutParams(
+            imageBorderSize.toInt(),
+            imageBorderSize.toInt()
+        ).apply {
+            gravity = Gravity.CENTER
+        }
+
+        val outerBorderCardView = CardView(context).apply {
+            layoutParams = outerLayoutParams
+            radius = outerBorderSize / 2 + 1f
+        }
+
+        val outerLayout = FrameLayout(context).apply {
+            layoutParams = outerLayoutParams
+            background = resources.getDrawable(R.drawable.gradient_color_2)
+        }
+
+        val innerBorderCardView = CardView(context).apply {
+            layoutParams = innerLayoutParams
+            setCardBackgroundColor(Color.WHITE)
+            radius = innerBorderSize / 2 + 1f
+        }
+
+        val imageBorderCardView = CardView(context).apply {
+            layoutParams = imageLayoutParams
+            setCardBackgroundColor(Color.TRANSPARENT)
+            radius = imageBorderSize / 2 + 1f
+        }
+
+        val imageView = ImageView(context).apply {
+            layoutParams = imageLayoutParams
+            if(imageResourceId != -1) setImageResource(imageResourceId)
+            scaleType = ImageView.ScaleType.CENTER_CROP
+        }
+
+        imageBorderCardView.addView(imageView)
+        innerBorderCardView.addView(imageBorderCardView)
+        outerLayout.addView(innerBorderCardView)
+        outerBorderCardView.addView(outerLayout)
+        addView(outerBorderCardView)
+
+        a.recycle()
     }
+
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
     )
 }
+
