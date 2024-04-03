12db67f0b125db34f9a45c2677119257889c589e
JUN <treeralph@gmail.com>
2024-04-03 14:02:03 +0900

    make click listener

--- app/src/main/java/kr/camp/sns/DetailActivity.kt
+++ app/src/main/java/kr/camp/sns/DetailActivity.kt
@@ -1,14 +1,48 @@
 package kr.camp.sns
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kr.camp.sns.databinding.ActivityMainBinding
+import kr.camp.sns.data.Posting
+import kr.camp.sns.data.User
+import kr.camp.sns.databinding.ActivityDetailBinding
 class DetailActivity : AppCompatActivity() {
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val binding by lazy { ActivityDetailBinding.inflate(layoutInflater) }
+    private val descriptionMaxLineNum = 2
+    private var isFolded = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        // val currentPosting = intent.getSerializableExtra("") as Posting
+        val currentPosting = intent.getSerializableExtra("") as Posting
+        val currentUser = intent.getSerializableExtra("") as User
 
+        initView(currentPosting, currentUser)
+    }
+
+    private fun initView(
+        currentPosting: Posting,
+        currentUser: User
+    ) {
+        binding.detailCurrentUserProfileTitleTextView.text = currentUser.name
+        binding.detailCurrentContentImageView.setImageResource(currentPosting.imageDrawableId)
+        binding.detailCurrentContentDescriptionTextView.text = currentPosting.description
+        binding.detailCurrentContentNumLikeTextView.text = "${currentPosting.likeCount} 좋아요"
+
+        binding.detailCurrentContentLikeImageView.run {
+            setOnClickListener {
+                currentPosting.switchLike(currentUser.id)
+                if(currentPosting.isLiked(currentUser.id)) {
+                    setImageResource(R.drawable.like_heart_icon)
+                }else setImageResource(R.drawable.like_icon)
+            }
+        }
+
+        binding.detailCurrentContentDescriptionTextView.run {
+            setOnClickListener {
+                maxLines = if(isFolded) Integer.MAX_VALUE else descriptionMaxLineNum
+                isFolded = !isFolded
+            }
+        }
     }
 }
--- app/src/main/java/kr/camp/sns/registry/UserRegistry.kt
+++ app/src/main/java/kr/camp/sns/registry/UserRegistry.kt
@@ -3,7 +3,6 @@ package kr.camp.sns.registry
 import kr.camp.sns.data.User
 
 class UserRegistry {
-
     companion object {
         private var userRegistry: UserRegistry? = null
 
@@ -11,17 +10,13 @@ class UserRegistry {
             return userRegistry ?: UserRegistry().apply { userRegistry = this }
         }
     }
-
     private val users = mutableListOf<User>()
-
     fun isUser(id: String): Boolean {
         return users.any { it.id == id }
     }
-
     fun addUser(user: User) {
         users.add(user)
     }
-
     fun findUserByIdAndPassword(id: String, password: String): User? {
         return users.find { it.id == id && it.password == password }
     }
