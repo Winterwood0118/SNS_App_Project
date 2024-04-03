d0bb0209420e0547001eddaff09651c86e70e718
JUN <treeralph@gmail.com>
2024-04-03 14:12:47 +0900

    edit getIntent

--- app/src/main/java/kr/camp/sns/DetailActivity.kt
+++ app/src/main/java/kr/camp/sns/DetailActivity.kt
@@ -14,25 +14,29 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val currentPosting = intent.getSerializableExtra("") as Posting
-        val currentUser = intent.getSerializableExtra("") as User
-
-        initView(currentPosting, currentUser)
+        try {
+            val currentPosting: Posting = intent.getSerializableExtra("extra_post") as Posting
+            val currentUser: String = intent.getStringExtra("extra_posting_user_name")!!
+            initView(currentPosting, currentUser)
+        } catch(e: Exception) {
+            finish()
+            // 게시물 열람 불가
+        }
     }
 
     private fun initView(
         currentPosting: Posting,
-        currentUser: User
+        currentUserName: String
     ) {
-        binding.detailCurrentUserProfileTitleTextView.text = currentUser.name
+        binding.detailCurrentUserProfileTitleTextView.text = currentUserName
         binding.detailCurrentContentImageView.setImageResource(currentPosting.imageDrawableId)
         binding.detailCurrentContentDescriptionTextView.text = currentPosting.description
         binding.detailCurrentContentNumLikeTextView.text = "${currentPosting.likeCount} 좋아요"
 
         binding.detailCurrentContentLikeImageView.run {
             setOnClickListener {
-                currentPosting.switchLike(currentUser.id)
-                if(currentPosting.isLiked(currentUser.id)) {
+                currentPosting.switchLike(currentUserName)
+                if(currentPosting.isLiked(currentUserName)) {
                     setImageResource(R.drawable.like_heart_icon)
                 }else setImageResource(R.drawable.like_icon)
             }
