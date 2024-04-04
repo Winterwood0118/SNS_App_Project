6fdcaca86997677e64f1d7744e01cf9067b3dfd6
minsu park <chchddr@naver.com>
2024-04-04 17:21:59 +0900

    중복확인 일부사항 수정

--- app/src/main/java/kr/camp/sns/SignUpActivity.kt
+++ app/src/main/java/kr/camp/sns/SignUpActivity.kt
@@ -14,10 +14,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kr.camp.sns.data.User
 import kr.camp.sns.databinding.ActivitySignUpBinding
+import kr.camp.sns.intent.IntentKey
 import kr.camp.sns.registry.UserRegistry
 import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
+    private val userRegistry = UserRegistry.getInstance()
 
     private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,20 +36,19 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(binding.root)
         val id = binding.signUpIdeditText.text
         val password = binding.signUpPasswordeditText.text
-        val RePassword = binding.signUpRepasswordeditText.text
+        val repassword = binding.signUpRepasswordeditText.text
         val name = binding.signUpNameeditText.text
-        var idCheck = 1
+        var idCheck = false
 
 
         binding.signUpIdcheckButton.setOnClickListener {
-            val userRegistry = UserRegistry.getInstance()
 
             if (id.isBlank()) {
                 Toast.makeText(this, "아이디를 입력해 주세요", Toast.LENGTH_SHORT).show()
             } else if (userRegistry.isUser(id.toString())) {
                 Toast.makeText(this, "이미 존재하는 아이디 입니다", Toast.LENGTH_SHORT).show()
             } else {
-                idCheck = 2
+                idCheck = true
                 binding.signUpIdcheckButton.isEnabled = false
                 binding.signUpIdeditText.isEnabled = false
                 binding.signUpIdcheckButton.text = "확인완료"
@@ -58,7 +59,6 @@ class SignUpActivity : AppCompatActivity() {
         }
 
         binding.signUpSignupButton.setOnClickListener {
-            val userRegistry = UserRegistry.getInstance()
             val user = User(id.toString(), password.toString(), name.toString())
 
             if (id.isBlank()) {
@@ -67,19 +67,18 @@ class SignUpActivity : AppCompatActivity() {
                 Toast.makeText(this, "이름을 입력해 주세요", Toast.LENGTH_SHORT).show()
             } else if (password.isBlank()) {
                 Toast.makeText(this, "비밀번호를 입력해 주세요", Toast.LENGTH_SHORT).show()
-            } else if (RePassword.isBlank()) {
+            } else if (repassword.isBlank()) {
                 Toast.makeText(this, "비밀번호를 한번 더 입력해 주세요", Toast.LENGTH_SHORT).show()
             } else if (password.toString()
-                != RePassword.toString()) {
+                != repassword.toString()) {
                 Toast.makeText(this, "비밀번호가 다릅니다", Toast.LENGTH_SHORT).show()
-            } else if (idCheck == 1) {
+            } else if (!idCheck) {
                 Toast.makeText(this, "아이디 중복확인을 해주세요", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
                 val intent = Intent().apply {
-                    putExtra("signed_id", id.toString())
-                    putExtra("signed_password", password.toString())
-                    putExtra("signed_name", name.toString())
+                    putExtra(IntentKey.USER, user)
+
                 }
                 userRegistry.addUser(user)
                 setResult(Activity.RESULT_OK, intent)
