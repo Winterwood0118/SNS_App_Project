3a117a91450710981597c0538be4df42f8ff51a9
WinterWood0118 <dltjdals125@gmail.com>
2024-04-05 20:32:25 +0900

    민수님 UserRegistry 임시로 추가

--- app/src/main/java/kr/camp/sns/SignUpActivity.kt
+++ app/src/main/java/kr/camp/sns/SignUpActivity.kt
@@ -84,19 +84,19 @@ class SignUpActivity : AppCompatActivity() {
         binding.signUpIdcheckButton.setOnClickListener {
 
             if (id.isBlank()) {
-                Toast.makeText(this, "아이디를 입력해 주세요", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.inputId), Toast.LENGTH_SHORT).show()
             } else if (userRegistry.isUser(id.toString())) {
-                Toast.makeText(this, "이미 존재하는 아이디 입니다", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.usedId), Toast.LENGTH_SHORT).show()
             } else if (!isRegularId(id.toString())) {
-                Toast.makeText(this, "올바른 아이디 형식이 아닙니다", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.isRegularId), Toast.LENGTH_SHORT).show()
             } else {
                 idCheck = true
                 binding.signUpIdcheckButton.isEnabled = false
                 binding.signUpIdeditText.isEnabled = false
-                binding.signUpIdcheckButton.text = "확인완료"
+                binding.signUpIdcheckButton.text = getString(R.string.checked)
                 binding.signUpIdcheckButton.background =
                     ContextCompat.getDrawable(this, R.drawable.btn_round2)
-                Toast.makeText(this, "사용 가능한 아이디 입니다", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.usableId), Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -104,23 +104,23 @@ class SignUpActivity : AppCompatActivity() {
             val user = User(id.toString(), password.toString(), name.toString())
 
             if (id.isBlank()) {
-                Toast.makeText(this, "아이디를 입력해 주세요", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.inputId), Toast.LENGTH_SHORT).show()
             } else if (name.isBlank()) {
-                Toast.makeText(this, "이름을 입력해 주세요", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.inputName), Toast.LENGTH_SHORT).show()
             } else if (password.isBlank()) {
-                Toast.makeText(this, "비밀번호를 입력해 주세요", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.inputPassword), Toast.LENGTH_SHORT).show()
             } else if (!isRegularPassword(password.toString())) {
-                Toast.makeText(this, "올바른 비밀번호 형식이 아닙니다", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.isRegularPassword), Toast.LENGTH_SHORT).show()
             } else if (repassword.isBlank()) {
-                Toast.makeText(this, "비밀번호를 한번 더 입력해 주세요", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.inputRePassword), Toast.LENGTH_SHORT).show()
             } else if (password.toString()
                 != repassword.toString()
             ) {
-                Toast.makeText(this, "비밀번호가 다릅니다", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.checkRePassword), Toast.LENGTH_SHORT).show()
             } else if (!idCheck) {
-                Toast.makeText(this, "아이디 중복확인을 해주세요", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.pleaseIdCheck), Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.signUpComplete), Toast.LENGTH_SHORT).show()
                 val intent = Intent().apply {
                     putExtra(IntentKey.USER, user)
                 }
